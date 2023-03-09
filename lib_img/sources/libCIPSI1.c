464dbfdc0c767d2ce010129d14ef66d55d43e6b1
Doriane <dorigood69@gmail.com>
2023-03-09 16:09:53 +0100

    Auto stash before rebase of "origin/main"

--- lib_img/sources/libCIPSI1.c
+++ lib_img/sources/libCIPSI1.c
@@ -12,15 +12,15 @@
 
 /* static -> non extern */
 
-int min(int a, int b)
-{
-	return (a < b) ? a : b;
-}
-
-int max(int a, int b)
-{
-	return (a > b) ? a : b;
-}
+//int min(int a, int b)
+//{
+//	return (a < b) ? a : b;
+//}
+//
+//int max(int a, int b)
+//{
+//	return (a > b) ? a : b;
+//}
 
 typedef struct
 {
