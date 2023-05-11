3ebb201fd93c80328848d4357ff6054fa357eadf
antoine <aduteyrat@gmail.com>
2023-05-11 14:36:24 +0200

    Adapt main to read all images

--- sources/main.c
+++ sources/main.c
@@ -1,3 +1,4 @@
+#include <dirent.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -26,26 +27,49 @@ int main(void)
 	{
 		case 1:
 			printf("\nNous allons traiter l'ensemble des images\n");
-			
-			// TODO : Traiter l'ensemble des images
-			
-			break;
+
+            // Ouvrir le dossier contenant les images source
+            DIR *dir;
+            struct dirent *ent;
+            if ((dir = opendir(sourceDir)) != NULL) {
+                /* parcourir tous les fichiers et répertoires dans le dossier */
+                while ((ent = readdir(dir)) != NULL) {
+                    if (strcmp(ent->d_name, ".") == 0 || strcmp(ent->d_name, "..") == 0) {
+                        continue;
+                    }
+                    // Générer le chemin complet de l'image source
+                    char filePath[200];
+                    strcpy(filePath, sourceDir);
+                    strcat(filePath, ent->d_name);
+                    printf("Traitement de l'image %s ...\n", filePath);
+
+                    // Charger l'image source
+                    IMAGE img = {0, 0, NULL, NULL};
+                    img = lectureImage(filePath);
+
+                    // TODO : Traiter l'image
+
+                    // Libérer la mémoire
+                    liberationImage(&img);
+				}
+			}
 		case 2:
 			printf("\nQuelle image souhaitez-vous traiter ? \nEntrez le nom de l'image (exemple : In_1.pgm)\t: ");
-			fileName = lectureString(10);
-			
-			strcat(sourceDir, fileName);
-			
-			IMAGE img = { 0,0,NULL,NULL };
-			img = allocationImage(200, 200);
-			img = lectureImage(sourceDir);
+            fileName = lectureString(10);
 
-			// TODO : Traiter l'image
-			
-			break;
+            strcat(sourceDir, fileName);
+
+            // Charger l'image source
+            IMAGE img = {0, 0, NULL, NULL};
+            img = lectureImage(sourceDir);
+
+            // TODO : Traiter l'image
+
+            // Libérer la mémoire
+            liberationImage(&img);
 		default:
 			printf("\nErreur de saisie.\n");
 	}
 
-
+	return EXIT_SUCCESS;
 }
